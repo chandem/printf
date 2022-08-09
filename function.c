9fc02d1c2a8ae3f28662f0fd2da4c55054a4cde1
chandem <49794721+chandem@users.noreply.github.com>
2022-08-09 15:55:57 +0300

    Update function.c

--- function.c
+++ function.c
@@ -146,3 +146,17 @@ int print_base16_upper_lower(va_list arg, char *representation)
 	cont = _strlen(s);
 	return (cont);
 }
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: number to be printed in binary
+ * @printed: hold the number of characters printed
+ */
+void print_binary(unsigned int n, unsigned int *printed)
+{
+	if (n > 1)
+	{
+		*printed += 1;
+		print_binary(n >> 1, printed);
+	}
+	_putchar((n & 1) + '0');
+}
