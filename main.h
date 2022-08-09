a84bedc7ec9972c2ebce9bb32a9267f260e11372
chandem <49794721+chandem@users.noreply.github.com>
2022-08-09 16:16:06 +0300

    Update main.h

--- main.h
+++ main.h
@@ -28,8 +28,8 @@ int check_percent(int *flag, char aux);
 int print_STR(va_list arg);
 int print_str(va_list arg);
 int print_unsignedIntToHex(unsigned int num, char _case);
-void print_binary(unsigned int n, unsigned int *printed);
 int print_oct(va_list arg);
 int print_unsignedToBinary(va_list arg);
+void print_binary(unsigned int n, unsigned int *printed);
 
 #endif
