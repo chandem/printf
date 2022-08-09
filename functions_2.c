93204a07cca7c8b7484ea738131c45883e8e2727
chandem <49794721+chandem@users.noreply.github.com>
2022-08-09 15:50:52 +0300

    Update functions_2.c

--- functions_2.c
+++ functions_2.c
@@ -1,6 +1,7 @@
 #include"main.h"
 #include<stdlib.h>
 #include<unistd.h>
+#include<stdarg.h>
 
 
 /**
@@ -99,4 +100,62 @@ int print_unsignedIntToHex(unsigned int num, char _case)
 		_putchar(numhex[j]);
 	free(numhex);
 	return (nbrCharacters);
+}
+
+/**
+ * print_unsignedToBinary - prints an integer.
+ * @arg: argument
+ * Return: 0
+ */
+int print_unsignedToBinary(va_list arg)
+{
+
+unsigned int n = va_arg(arg, unsigned int);
+unsigned int printed;
+
+print_binary(n, &printed);
+print_binary(n, &printed);
+
+return (printed);
+}
+
+
+/**
+ * print_oct - prints number in octal base.
+ * @arg: list containing octal number to be printed
+ * Return: number of octals printed
+ */
+
+int print_oct(va_list arg)
+{
+	unsigned int num = va_arg(arg, unsigned int);
+	unsigned int copy;
+	char *octa;
+	int i, j, charPrinted = 0;
+
+	if (num == 0)
+		return (_putchar('0'));
+	for (copy = num; copy != 0; j++)
+	{
+		copy = copy / 8;
+	}
+	octa = malloc(j);
+	if (!octa)
+		return (-1);
+
+	for (i = j - 1; i >= 0; i--)
+	{
+		octa[i] = num % 8 + '0';
+		num = num / 8;
+	}
+
+	for (i = 0; i < j && octa[i] == '0'; i++)
+		;
+	for (; i < j; i++)
+	{
+		_putchar(octa[i]);
+		charPrinted++;
+	}
+	free(octa);
+	return (charPrinted);
 }
